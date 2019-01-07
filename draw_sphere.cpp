41964623d03466f15bba188def4cc6b79a4e22aa
WarrenGreen <s.green.warren@gmail.com>
2019-01-07 15:49:07 -0800

    Add a sphere to the world

--- draw_sphere.cpp
+++ draw_sphere.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include "ray.h"
 
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
 vec3 color(const ray& r) {
+    vec3 red = vec3(1, 0, 0);
     vec3 white = vec3(1.0, 1.0, 1.0);
     vec3 light_blue = vec3(0.5, 0.7, 1.0);
+    vec3 sphere_center = vec3(0, 0, -1);
+    if (hit_sphere(sphere_center, 0.5, r)) {
+        return red;
+    }
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*white + t*light_blue;
